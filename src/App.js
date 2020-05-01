dde45b351d1997936273c3a76da2ee47a68053bc
soo.toance <soo.toance@gmail.com>
2020-05-01 21:33:09 +0900

    map 사용하는 두 가지 방법 + propTypes 예시 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // props.fav = {fav} : es6 문법 
-function Food({ fav }) {
-  return <h3>I love {fav}!</h3>;
+function Food({ fav, picture, rating }) {
+  return <div>
+        <h3>I love {fav}!</h3>
+        <h4>{rating} / 10</h4>
+        <img src={picture} alt={fav}></img>
+  </div> 
 }
 
-// food component에게 fav property(prop)로 potato 전달
-// boolean, array 모두 전달 가능 
+Food.propTypes = {
+  fav: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+
+const foodIlike = [
+  {
+    id: 1,
+    name: "potato",
+    image: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    rating: 5.5
+  },
+  {
+    id: 2,
+    name: "carrot",
+    image: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&auto=format&fit=crop&w=1161&q=80",
+    rating: 4.6  
+  },
+  {
+    id: 3,
+    name: "ham",
+    image: "https://images.unsplash.com/photo-1524438418049-ab2acb7aa48f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    rating: 4.3 
+  }
+];
+
+function renderFood(dish) {
+  return <Food 
+    key={dish.id} 
+    fav={dish.name} 
+    picture={dish.image} 
+  />
+}
+
+
+// 방법1
+//   {foodIlike.map((dish) => (
+//   <Food fav={dish.name} picture={dish.image}/>
+//   ))}
+
+// 방법 2 
+// <div>
+// {foodIlike.map(renderFood)}
+// </div>
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="potato" /> 
-      <Food fav="carrot" /> 
-      <Food fav="ham" /> 
+      {foodIlike.map((dish) => (
+        <Food key={dish.id} fav={dish.name} picture={dish.image} rating={dish.rating}/>
+      ))}
     </div>
   );
 }
