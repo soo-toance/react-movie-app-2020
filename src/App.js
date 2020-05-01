407cccb3831e1048efe996ec489c795a2bd28930
soo.toance <soo.toance@gmail.com>
2020-05-01 20:54:14 +0900

    prop으로 컴포넌트에게 데이터 전달하기
    - Potato.js App.js로 통합
    - prop으로 컴포넌트에 데이터 전달

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Potato from "./Potato";
 
+// props.fav = {fav} : es6 문법 
+function Food({ fav }) {
+  return <h3>I love {fav}!</h3>;
+}
+
+// food component에게 fav property(prop)로 potato 전달
+// boolean, array 모두 전달 가능 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato/>
+      <Food fav="potato" /> 
+      <Food fav="carrot" /> 
+      <Food fav="ham" /> 
     </div>
   );
 }
