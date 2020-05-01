2fb8145cb8f9cf2ed7647ae3421d4ec808162ac4
soo.toance <soo.toance@gmail.com>
2020-05-01 22:18:42 +0900

    componentDidMount 이용해 loading 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,29 +4,18 @@ import PropTypes from 'prop-types';
 class App extends React.Component {
   // 변하는 data는 state에 추가 
   state = {
-    count: 0
+    isLoading: true
   };
 
-  add= () => {
-    // this.state.count = 1; -- react가 state 자동 새로고침 하지 않음.
-    // setState 통해 새로고침 유도해야함. - 자동으로 render 호출 (rerender)
-
-
-    // 참고 : 아래 방식은 performance 이슈 존재함
-    // this.setState({ count: this.state.count + 1 });
-    
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus= () => {
-    this.setState(current => ({ count: current.count - 1}));
-  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState ({ isLoading: false });
+    }, 3000);
+  }
 
   render() {
-    return <div> 
-      <h1> The number is :  {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-      </div>
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
   }
 
 }
